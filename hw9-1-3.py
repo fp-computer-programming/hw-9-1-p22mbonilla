c81bd32d4df51b447a5a33bfcce19cb37b5336b8
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-01-14 10:19:19 -0500

    add file

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -3,4 +3,11 @@
 def find_thing(search,look):
     found = -1
     for i, v in enumerate(search):
-        if i == 
+        if v == look[0]:
+            num = 1
+            check = ''
+            for x in range(len(search)):
+                check += search[num]
+                num += 1
+
+            return i
